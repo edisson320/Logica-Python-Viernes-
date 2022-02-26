d158f1d5364939d65e1b02dcc95d2cdea0c4152e
edisson320 <edisson320@yahoo.es>
2022-02-25 20:40:32 -0500

    NTV001 se ingreso un nuevo comentario el cual es repazo de lo aprendido

--- main.py
+++ main.py
@@ -2,3 +2,6 @@
 print ("Que miedo la guerra")
 nivelAgua = int(input("Digita la Cantiad de agua de la represa: "))
 print(f"El nivel de agua es {nivelAgua}")
+
+#Se Agrega un comentario 
+print("Estamos haciendo un repazo nuevo")
